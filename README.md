65060686141f414212c1ab817bbcf8e6a102aeb2
marnvel <marnvel.24@gmail.com>
2024-03-09 09:27:16 -0300

    Se crean algunos métodos. Se deja la estructura más o menos armada. 09-03-2024. 09:27hs

---
+++ README.md
@@ -0,0 +1,53 @@
+# Juego de consola de interfaz básica con `C#`
+
+> Este proyecto se trata de una simple aplicación de consola creada con el framework dotNET.
+>
+> El objetivo de este proyecto es realizar prácticas para aprender **C#** y el marco de
+> trabajo **.NET-CORE**.
+
+## Sobre la APP
+
+> Este es un juego de consola. Es una simple aplicación de consola que consiste en "comer" un determinado alimento por parte de una jugador mostrado en pantalla. Las principales características son:
+>
+
+## About the APP
+
+## Tecnologías utilizadas:
+<div align="center" style="display: flex">
+      <span>
+         <a href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank">
+               <img width="100" style="margin: 10" title='cSharp' src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png'>
+         </a>
+      </span>
+      <span>
+         <a href="https://learn.microsoft.com/en-us/dotnet/" target="_blank" title='dotNET'>
+               <img width="100" style="margin: 10" title='dotNET' src='https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg'>
+         </a>
+      </span>
+</div>
+
+## Requerimientos
+
+- Tener instalado **Visual Studio Code**.
+- Tener intalado el sdk **dotNET 8** (<https://dotnet.microsoft.com/en-us/download>).
+- Tener instalado **Git**.
+
+## Usos
+
+### Para ejecutar o utilizar el proyecto debes clonar este repositorio en un directorio de tu sistema. Abre una terminal y ejecuta el siguiente comando
+
+```bash
+git clone https://github.com/MARnVEL/cSharpConsoleUIGame.git
+```
+
+### Con una terminal localizarse en el directorio del proyecto y ejecutar
+
+```bash
+dotnet build
+```
+
+### Para ejecutar el proyecto
+
+```bash
+dotnet run
+```
--- cSharpConsoleUIGame/Program.cs
+++ cSharpConsoleUIGame/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Random random = new();
+Console.CursorVisible = false;
+int altura = Console.WindowHeight - 1;
+int ancho = Console.WindowWidth - 1;
+bool deberiaSalir = false;
+
+// Posición en la consola del jugador:
+int jugadorX = 0;
+int jugadorY = 0;
+
+// Posición en la consola de la comida:
+int comidaX = 0;
+int comidaY = 0;
+
+// Strings disponibles para el jugador y la comida:
+string[] estados = ["('-')", "(^-^)", "(X_X)"];
+string[] comidas = ["@@@@@", "$$$$$", "#####"];
+
+// String actual del juagador mostrada en la consola
+string aparienciaJugador = estados[0];
+
+// Indice de la apariencia actual de la comida:
+int indiceComidaActual = 0;
+
+IniciarJuego();
+
+while (!deberiaSalir)
+{
+    if (TerminalCambiada())
+    {
+        Console.Clear();
+        Console.WriteLine("La consola fué cambiada de tamaño. Saliendo...");
+        deberiaSalir = true;
+    }
+    else
+    {
+        Moverse();
+    }
+}
+
+// Mostrar una comida aleatoria en una posición aleatoria
+void MostrarComida()
+{
+     // Actualizar la comida a un índice aleatoria.
+    indiceComidaActual = random.Next(0, comidas.Length);
+
+    // Update food position to a random location
+    comidaX = random.Next(0, ancho - aparienciaJugador.Length);
+    comidaY = random.Next(0, altura - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(comidaX, comidaY);
+    Console.Write(comidas[indiceComidaActual]);
+}
+
+// Devuelve true si la terminal cambia de tamaño. 
+bool TerminalCambiada() 
+{
+    return altura != Console.WindowHeight - 1 || ancho != Console.WindowWidth - 5;
+}
+
+// Limpiamos la consola, mostrar la comida y el jugador
+void IniciarJuego()
+{
+    Console.Clear();
+
+    MostrarComida();
+    Console.SetCursorPosition(0,0);
+    Console.Write(aparienciaJugador);
+}
+
